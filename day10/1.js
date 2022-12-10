d4a928bde5068f6261e517eb9a1170008f2bfe73
Vincent de Bruijn <vincent.de.bruijn@flock.community>
2022-12-10 12:17:22 +0100

    day10

---
+++ day10/1.js
@@ -0,0 +1,185 @@
+let text = await fetch("https://adventofcode.com/2022/day/10/input").then(i => i.text());
+
+// let text = 
+// `addx 15
+// addx -11
+// addx 6
+// addx -3
+// addx 5
+// addx -1
+// addx -8
+// addx 13
+// addx 4
+// noop
+// addx -1
+// addx 5
+// addx -1
+// addx 5
+// addx -1
+// addx 5
+// addx -1
+// addx 5
+// addx -1
+// addx -35
+// addx 1
+// addx 24
+// addx -19
+// addx 1
+// addx 16
+// addx -11
+// noop
+// noop
+// addx 21
+// addx -15
+// noop
+// noop
+// addx -3
+// addx 9
+// addx 1
+// addx -3
+// addx 8
+// addx 1
+// addx 5
+// noop
+// noop
+// noop
+// noop
+// noop
+// addx -36
+// noop
+// addx 1
+// addx 7
+// noop
+// noop
+// noop
+// addx 2
+// addx 6
+// noop
+// noop
+// noop
+// noop
+// noop
+// addx 1
+// noop
+// noop
+// addx 7
+// addx 1
+// noop
+// addx -13
+// addx 13
+// addx 7
+// noop
+// addx 1
+// addx -33
+// noop
+// noop
+// noop
+// addx 2
+// noop
+// noop
+// noop
+// addx 8
+// noop
+// addx -1
+// addx 2
+// addx 1
+// noop
+// addx 17
+// addx -9
+// addx 1
+// addx 1
+// addx -3
+// addx 11
+// noop
+// noop
+// addx 1
+// noop
+// addx 1
+// noop
+// noop
+// addx -13
+// addx -19
+// addx 1
+// addx 3
+// addx 26
+// addx -30
+// addx 12
+// addx -1
+// addx 3
+// addx 1
+// noop
+// noop
+// noop
+// addx -9
+// addx 18
+// addx 1
+// addx 2
+// noop
+// noop
+// addx 9
+// noop
+// noop
+// noop
+// addx -1
+// addx 2
+// addx -37
+// addx 1
+// addx 3
+// noop
+// addx 15
+// addx -21
+// addx 22
+// addx -6
+// addx 1
+// noop
+// addx 2
+// addx 1
+// noop
+// addx -10
+// noop
+// noop
+// addx 20
+// addx 1
+// addx 2
+// addx 2
+// addx -6
+// addx -11
+// noop
+// noop
+// noop`;
+
+let instructions = text.trim().split('\n');
+
+let cycleFunc = (cycle) => (cycle - 20) % 40 === 0;
+let total = 0;
+let x = 1;
+let instruction = null;
+let addCount = 0;
+
+for(let cycle = 1; cycle <= 220; cycle++) {
+    if (cycleFunc(cycle)) {
+        console.log("TOTAL UPDATE")
+        console.log(x);
+        console.log(cycle);
+        console.log(x*cycle);
+        total += x*cycle;
+        console.log(total);
+    }
+
+    if (instruction === null) {
+        instruction = instructions.shift();
+    }
+    if (instruction === 'noop') {
+       instruction = null;
+    } else if (match = instruction.match(/addx (-?\d+)/)) {
+        if (addCount === 0) {
+            addCount = 1;
+        } else {
+            x += Number(match[1]);
+            addCount = 0;
+            instruction = null;
+        }
+    }
+}
+
+console.log(total);
---
+++ day10/2.js
@@ -0,0 +1,188 @@
+let text = await fetch("https://adventofcode.com/2022/day/10/input").then(i => i.text());
+
+// let text = 
+// `addx 15
+// addx -11
+// addx 6
+// addx -3
+// addx 5
+// addx -1
+// addx -8
+// addx 13
+// addx 4
+// noop
+// addx -1
+// addx 5
+// addx -1
+// addx 5
+// addx -1
+// addx 5
+// addx -1
+// addx 5
+// addx -1
+// addx -35
+// addx 1
+// addx 24
+// addx -19
+// addx 1
+// addx 16
+// addx -11
+// noop
+// noop
+// addx 21
+// addx -15
+// noop
+// noop
+// addx -3
+// addx 9
+// addx 1
+// addx -3
+// addx 8
+// addx 1
+// addx 5
+// noop
+// noop
+// noop
+// noop
+// noop
+// addx -36
+// noop
+// addx 1
+// addx 7
+// noop
+// noop
+// noop
+// addx 2
+// addx 6
+// noop
+// noop
+// noop
+// noop
+// noop
+// addx 1
+// noop
+// noop
+// addx 7
+// addx 1
+// noop
+// addx -13
+// addx 13
+// addx 7
+// noop
+// addx 1
+// addx -33
+// noop
+// noop
+// noop
+// addx 2
+// noop
+// noop
+// noop
+// addx 8
+// noop
+// addx -1
+// addx 2
+// addx 1
+// noop
+// addx 17
+// addx -9
+// addx 1
+// addx 1
+// addx -3
+// addx 11
+// noop
+// noop
+// addx 1
+// noop
+// addx 1
+// noop
+// noop
+// addx -13
+// addx -19
+// addx 1
+// addx 3
+// addx 26
+// addx -30
+// addx 12
+// addx -1
+// addx 3
+// addx 1
+// noop
+// noop
+// noop
+// addx -9
+// addx 18
+// addx 1
+// addx 2
+// noop
+// noop
+// addx 9
+// noop
+// noop
+// noop
+// addx -1
+// addx 2
+// addx -37
+// addx 1
+// addx 3
+// noop
+// addx 15
+// addx -21
+// addx 22
+// addx -6
+// addx 1
+// noop
+// addx 2
+// addx 1
+// noop
+// addx -10
+// noop
+// noop
+// addx 20
+// addx 1
+// addx 2
+// addx 2
+// addx -6
+// addx -11
+// noop
+// noop
+// noop`;
+
+let instructions = text.trim().split('\n');
+
+let x = 1;
+let instruction = null;
+let addCount = 0;
+let rows = [];
+
+for(let cycle = 1; cycle <= 240; cycle++) {
+    let index = (cycle - 1) % 40;
+    if (index === 0) {
+        rows.push("");
+    }
+
+    let row = rows[rows.length - 1];
+    if (Math.abs(x - index) <= 1) {
+        row += '#';
+    } else {
+        row += '.';
+    }
+    rows[rows.length - 1] = row;
+
+    if (instruction === null) {
+        instruction = instructions.shift();
+    }
+    if (instruction === 'noop') {
+       instruction = null;
+    } else if (match = instruction.match(/addx (-?\d+)/)) {
+        if (addCount === 0) {
+            addCount = 1;
+        } else {
+            x += Number(match[1]);
+            addCount = 0;
+            instruction = null;
+        }
+    }
+}
+
+console.log(rows.join('\n'));
